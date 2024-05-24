69aa27144424a04d6a8940aa57c6d5d9434db1b0
sugutt <sugutmanu@gmail.com>
2024-05-24 14:17:56 +0300

    predict pipeline

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+
+app=application
+
+#route for a homepage
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    
+    else:
+        pass
+
+
